7fd6a37f97e83843fb5db74031abe325b7f3ba04
sicalo330 <noah.esteban9@gmail.com>
2024-09-25 14:04:43 -0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Ventas from './Screen/Ventas';
+
 export default function App() {
+  const Tab = createBottomTabNavigator()
+  const Stack = createStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </Tab.Navigator>
+      <Tab.Screen name='Ventas' component={Ventas}></Tab.Screen>
+      <Tab.Screen name='Ordenes'></Tab.Screen>
+      <Tab.Screen name='Inventario'></Tab.Screen>
+      <Tab.Screen name='Cuenta'></Tab.Screen>
+    </NavigationContainer>
   );
 }
 
---
+++ Screen/Ventas.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+function Ventas(){
+    return(
+        <Text>En ventas</Text>
+    )
+}
+
+export default Ventas
